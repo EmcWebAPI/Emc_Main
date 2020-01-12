e6e57892c31619d7d2577f1d22ca1ad33c01bb12
sun198986 <334828328@qq.com>
2020-01-12 23:05:44 +0800

    修改上传文件方法

--- EmcReportWebApi/Models/ReportResult.cs
+++ EmcReportWebApi/Models/ReportResult.cs
@@ -5,8 +5,12 @@ using System.Web;
 
 namespace EmcReportWebApi.Models
 {
-    public class ReportResult
+    public class ReportResult<T>
     {
+        public object Content { get; set; }
 
+        public string Message { get; set; }
+
+        public bool SumbitResult { get; set; }
     }
 }

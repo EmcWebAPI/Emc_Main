03af2e497830d6d9e3579be2942eaa54d2050d54
zy71 <zhangy.osb@vsc.com>
2020-01-17 16:33:06 +0800

    添加文件压缩的方法

---
+++ EmcReportWebApi/Common/ZipFileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.IO.Compression;
+
+namespace EmcReportWebApi.Common
+{
+    public static class ZipFileHelper
+    {
+        public static void DecompressionZip(string zipPath, string outputDirectory) {
+            DirectoryInfo di = new DirectoryInfo(outputDirectory);
+            if (!di.Exists) { di.Create(); }
+            ZipFile.ExtractToDirectory(zipPath, outputDirectory);
+        }
+
+        public static void CreateFromDirectoryZip(string inputDirectory, string zipPath) {
+            if (!Directory.Exists(inputDirectory)) {
+                throw new Exception("文件夹路径不存在");
+            }
+            ZipFile.CreateFromDirectory(inputDirectory, zipPath);
+        }
+    }
+}

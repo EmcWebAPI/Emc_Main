ade6bb87f787c475de6d41248c92a17bc05cd88a
sun198986 <334828328@qq.com>
2020-01-19 18:09:51 +0800

    添加文件操作的类

---
+++ EmcReportWebApi/Common/FileUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace EmcReportWebApi.Common
+{
+    public static class FileUtil
+    {
+        //根据报告id创建报告文件夹
+        public static string CreateReportDirectory(string filePath)
+        {
+            string datetimeStr = DateTime.Now.ToString("yyyyMMddhhmmss");
+            string outputPath = string.Format("{0}\\{1}", filePath, datetimeStr);
+            if (Directory.Exists(outputPath))
+            {
+                throw new Exception("文件夹已经存在");
+            }
+            else
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+            return outputPath;
+        }
+    }
+}
